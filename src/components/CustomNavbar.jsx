2b96664376a08f4f592443b7ff4e9e26fa2672a9
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2023-09-23 14:31:02 +0530

    Component

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -6,6 +6,9 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { useContext } from "react";
 import { toast } from "react-toastify";
+import Image from 'next/image'; // Import the Image component
+
+import logo from "../assets/logo.png"
 
 const CustomNavbar = () => {
   const context = useContext(UserContext);
@@ -24,61 +27,69 @@ const CustomNavbar = () => {
   }
 
   return (
-    <nav className="bg-blue-600 h-16 py-2 px-36 flex justify-between items-center">
-      <div className="brand">
-        <h1 className="text-2xl font-semibold">
-          <a href="#!">Work Manager</a>
-        </h1>
-      </div>
-      <div>
-        <ul className="flex space-x-5">
-          {context.user && (
-            <>
-              <li>
-                <Link href={"/"} className="hover:text-blue-200">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link href="/add-task" className="hover:text-blue-200">
-                  Add Task
-                </Link>
-              </li>
-              <li>
-                <Link href={"/show-tasks"} className="hover:text-blue-200">
-                  Show Tasks
-                </Link>
-              </li>
-            </>
-          )}
-        </ul>
-      </div>
-      <div>
-        <ul className="flex space-x-3">
-          {context.user && (
-            <>
-              <li>
-                <Link href={"#!"}>{context.user.name}</Link>
-              </li>
-              <li>
-                <button onClick={doLogout}>Logout</button>
-              </li>
-            </>
-          )}
 
-          {!context.user && (
-            <>
-              <li>
-                <Link href="/login">Login</Link>
-              </li>
-              <li>
-                <Link href="/signup">Signup</Link>
-              </li>
-            </>
-          )}
-        </ul>
-      </div>
-    </nav>
+<nav className="h-16 py-2 px-36 flex justify-between items-center">
+  <div className="brand flex items-center">
+    {/* Add the brand logo image using Next.js's Image component */}
+    <Image
+      src={logo} // Replace with your actual image path
+      alt="Work Manager Logo"
+      width={130} // Adjust width as needed
+      height={20} // Adjust height as needed
+    />
+
+  </div>
+  <div>
+    <ul className="flex space-x-5">
+      {context.user && (
+        <>
+          <li>
+            <Link href={"/"} className="hover:text-blue-200">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/add-task" className="hover:text-blue-200">
+              Add Task
+            </Link>
+          </li>
+          <li>
+            <Link href={"/show-tasks"} className="hover:text-blue-200">
+              Show Tasks
+            </Link>
+          </li>
+        </>
+      )}
+    </ul>
+  </div>
+  <div>
+    <ul className="flex space-x-3">
+      {context.user && (
+        <>
+          <li>
+            <Link href={"#!"}>{context.user.name}</Link>
+          </li>
+          <li>
+            <button onClick={doLogout}>Logout</button>
+          </li>
+        </>
+      )}
+
+      {!context.user && (
+        <>
+          <li>
+            <Link href="/login">Login</Link>
+          </li>
+          <li>
+            <Link href="/signup">Signup</Link>
+          </li>
+        </>
+      )}
+    </ul>
+  </div>
+</nav>
+
+
   );
 };
 
